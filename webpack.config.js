f0567eeb89773eb2a5339480039596b3266762f2
erase2004 <erase2004@gmail.com>
2022-01-09 10:14:29 +0800

    1. first version (support RWD but lack of JavaScript function)

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [new MiniCssExtractPlugin()]
+};
